9f4ca05435c6ed4a1eee661a4b28634a5b06398b
NoahwTaylor <noah.taylor1@edu.sait.ca>
2025-01-17 14:59:08 -0700

    Add files via upload

---
+++ Unit0LabApp/Unit0Lab/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Unit0Lab
+ * Author: Noah
+ * When: Jan 2025
+ */
+
+using Unit0Lab;
+using System.Runtime.InteropServices.Marshalling;
+using Unit0Lab;
+
+int low, high;
+int difference; // difference between low and  high
+
+// get positive low number 
+// get high number that is greater than low
+
+low = Utilities.GetPositiveInt("low number");
+high = Utilities.GetIntInRange("high number", low + 1, Int32.MaxValue);
+
+// calculate and print difference between low and high
+difference = high - low;
+Console.WriteLine($"\ndifference between {low} and {high} is {difference}");
+
+// create an array and store number between low and high
+int[] numbers = new int[difference + 1];
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = low + i;
+}
+
+Console.WriteLine("Numbers in the array:");
+foreach (int num in numbers)
+    Console.WriteLine(num);
+
+// create a text file and write to it the numbers from the array in reverse order
+StreamWriter streamWriter = File.CreateText("numbers.txt");
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    streamWriter.WriteLine(numbers[i]);
+}
+streamWriter.Close(); // Close file writing
+Console.WriteLine("\nFile written");
+
+// read the numbers back from the file and print out the sum of the numbers
+StreamReader streamReader = File.OpenText("numbers.txt");
+string nextLine;
+for (int i = 0; i < numbers.Length; i++)
+{
+    nextLine = streamReader.ReadLine() ?? "";
+    numbers[i] = Convert.ToInt32(nextLine);
+}
+streamReader.Close();
+
+Console.WriteLine("Values from file");
+foreach (int num in numbers)
+    Console.WriteLine(num);
+
+int sum = numbers.Sum(); // Calculate Sum of Values in Nums Array
+Console.WriteLine($"\nSum: {sum}");
---
+++ Unit0LabApp/Unit0Lab/Utilities.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit0Lab
+{
+    /// <summary>
+    /// a repository of static methods for reading data and useful calculation
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// reads positive decimal value
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>positive decimal</returns>
+        public static decimal GetPositiveDecimal(string prompt)
+        {
+            bool gotIt = false;
+            decimal result = 0;
+            string input;
+            while (!gotIt) // loops while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}:"); // interpolated string
+                input = Console.ReadLine() ?? "";
+                if (Decimal.TryParse(input, out result))
+                //if parse successful, the resulting value is put in the second
+                //  arg and true returned
+                //if not successful, returns false
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else // not a decimal
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads positive int value
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <returns>positive int</returns>
+        public static int GetPositiveInt(string prompt)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+            while (!gotIt) // loops while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}:"); // interpolated string
+                input = Console.ReadLine() ?? "";
+                if (Int32.TryParse(input, out result))
+                //if parse successful, the resulting value is put in the second
+                //  arg and true returned
+                //if not successful, returns false
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else // not an int
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads int value within range
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <param name="minValue">minimum value for the range</param>
+        /// <param name="maxValue">maximum value for the range</param>
+        /// <returns>int value in range</returns>
+        public static int GetIntInRange(string prompt, int minValue, int maxValue)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+            while (!gotIt) // loops while gotIt is false
+            {
+                Console.WriteLine($"Enter {prompt}:"); // interpolated string
+                input = Console.ReadLine() ?? "";
+                if (Int32.TryParse(input, out result))
+                //if parse successful, the resulting value is put in the second
+                //  arg and true returned
+                //if not successful, returns false
+                {
+                    if (result >= minValue && result <= maxValue)
+                    {
+                        gotIt = true; // success
+                    }
+                    else
+                    {
+                        if (maxValue == Int32.MaxValue)
+                            Console.WriteLine($"Error: {prompt} must be greater than or equal to {minValue}");
+                        else if (minValue == Int32.MinValue)
+                            Console.WriteLine($"Error: {prompt} must be less than or equal to {maxValue}");
+                        else
+                            Console.WriteLine($"Error: {prompt} must be between {minValue} and {maxValue}");
+                    }
+                }
+                else // not an int
+                {
+                    Console.WriteLine($"Error: {prompt} must be a whole number");
+                }
+            }
+            return result;
+        }
+
+    }// class
+}// namespace
